7f73483dac40c3d3cb13726344cd5e40f4a2efec
Rushikesh <girirushikesh2@gmail.com>
2022-02-23 12:26:53 +0530

    first commit

---
+++ task-one/src/app/home/home.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    const openModalButtons = document.querySelectorAll('[data-modal-target]')
+    const closeModalButtons = document.querySelectorAll('[data-close-button]')
+    const overlay = document.getElementById('overlay')
+
+    openModalButtons.forEach(button =>  {
+      button.addEventListener('click', () =>{
+        const modalone  = document.getElementById('modalone')
+        openModal(modalone)
+      })
+    })
+
+    overlay?.addEventListener('click', () =>{
+      const modals = document.querySelectorAll('.modalone.active')
+      modals.forEach(modalone =>{
+        closeModal(modalone)
+      })
+    })
+
+    closeModalButtons.forEach(button =>  {
+      button.addEventListener('click', () =>{
+        const modalone = button.closest('.modalone')
+        closeModal(modalone)
+      })
+    })
+
+    function openModal(modalone:any){
+      if (modalone == null) return
+      modalone.classList.add('active')
+      overlay?.classList.add('active')
+    }
+
+    function closeModal(modalone:any){
+      if (modalone == null) return
+      modalone.classList.remove('active')
+      overlay?.classList.remove('active')
+    }
+
+  }
+
+}
