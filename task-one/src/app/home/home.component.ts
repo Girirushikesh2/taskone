a8c12b50eb24f635c492fe72a9e61f3dfe04f0e5
Rushikesh <girirushikesh2@gmail.com>
2022-02-25 11:58:33 +0530

    footer added

--- task-one/src/app/home/home.component.ts
+++ task-one/src/app/home/home.component.ts
@@ -48,6 +48,28 @@ export class HomeComponent implements OnInit {
       overlay?.classList.remove('active')
     }
 
+
+
+    // document.addEventListener('click',e=>{
+    //   const isDropdownButton = e.document.getElementById('alpha')
+    //   const isDropdownButton = e.target?.matches("[data-dropdown-button]")
+    //   if(!isDropdownButton && e.target?.closest('[data-dropdown]')!=null)return
+
+    //   let currentDropdown:any
+    //   if(isDropdownButton){
+    //     currentDropdown = e.target.closest('[data-dropdown]')
+    //     currentDropdown.classList.toggle('active')
+    //   }
+
+    //   document.querySelectorAll("[data-dropdown].active").forEach(dropdown =>{
+    //     if(dropdown === currentDropdown)return
+    //     dropdown.classList.remove('active')
+    //   })
+
+
+    // }
+    // )
+
   }
 
 }
